ce3c82a1f134cf09b809b0dc6aa4cf0a8fb571fb
Nick Corti <nacorti@gmail.com>
2015-07-16 20:56:08 -0500

    Working Google Play Services Sign-in, with consent screen.

--- app/src/main/java/com/corti/mapsandboxer/MainActivity.java
+++ app/src/main/java/com/corti/mapsandboxer/MainActivity.java
@@ -1,17 +1,78 @@
 package com.corti.mapsandboxer;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
 
+    private static final String STATE_RESOLVING_ERROR = "resolving_error";
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mResolvingError = savedInstanceState != null && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGoogleApiClient.connect();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!mResolvingError) {
+            //mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -35,4 +96,82 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Context context = getApplicationContext();
+        CharSequence text = "Sign in Successful! Confuckingratulations!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast signOnToast = Toast.makeText(context, text, duration);
+        signOnToast.show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(mResolvingError) {
+            return;
+        } else {
+            if (connectionResult.hasResolution()) {
+                try {
+                    mResolvingError = true;
+                    connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+                } catch (IntentSender.SendIntentException e) {
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                showErrorDialog(connectionResult.getErrorCode());
+                mResolvingError = true;
+            }
+        }
+    }
+
+    private void showErrorDialog(int errorCode) {
+        //create fragment
+        ErrorDialogFragment edf = new ErrorDialogFragment();
+        //pass error to display
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        edf.setArguments(args);
+        edf.show(getFragmentManager(), "errorDialog");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==REQUEST_RESOLVE_ERROR) {
+            mResolvingError= false;
+            if(resultCode == RESULT_OK) {
+                if(!mGoogleApiClient.isConnected() && !mGoogleApiClient.isConnecting()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() {
+
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode, this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MainActivity)getActivity()).onDialogDismissed();
+        }
+    }
+
+
 }
